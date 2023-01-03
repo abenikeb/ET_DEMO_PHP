38d7097543eef5b4458146c8a2f0390a0383f87b
abenikeb <abenikeb79@gmail.com>
2023-01-03 04:35:44 -0800

    modified:   API_SERVER/utils/tool.php

--- API_SERVER/utils/tool.php
+++ API_SERVER/utils/tool.php
@@ -72,34 +72,35 @@ function sortedString($stringApplet){
         }  
     }
 
-    echo 'START_';
-    print_r($stringExplode);
-    echo 'END_';
-
     return $stringExplode;
-
-   //return "appid=930231098009602&business_type=BuyGoods&merch_code=101011&merch_order_id=1472575010&method=payment.preorder&nonce_str=A2Y0OMG8E9H8TM5F45WR1V8VY78G6O5U&notify_url=https://www.google.com&payee_identifier=220311&payee_identifier_type=04&payee_type=5000&timeout_express=120m&timestamp=1672575010&title=diamond_20&total_amount=20&trade_type=InApp&trans_currency=ETB&version=1.0";
 }
 
 function SignWithRSA($data){
-   //$privateKey_pem = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC/ZcoOng1sJZ4CegopQVCw3HYqqVRLEudgT+dDpS8fRVy7zBgqZunju2VRCQuHeWs7yWgc9QGd4/8kRSLY+jlvKNeZ60yWcqEY+eKyQMmcjOz2Sn41fcVNgF+HV3DGiV4b23B6BCMjnpEFIb9d99/TsjsFSc7gCPgfl2yWDxE/Y1B2tVE6op2qd63YsMVFQGdre/CQYvFJENpQaBLMq4hHyBDgluUXlF0uA1X7UM0ZjbFC6ZIB/Hn1+pl5Ua8dKYrkVaecolmJT/s7c/+/1JeN+ja8luBoONsoODt2mTeVJHLF9Y3oh5rI+IY8HukIZJ1U6O7/JcjH3aRJTZagXUS9AgMBAAECggEBALBIBx8JcWFfEDZFwuAWeUQ7+VX3mVx/770kOuNx24HYt718D/HV0avfKETHqOfA7AQnz42EF1Yd7Rux1ZO0e3unSVRJhMO4linT1XjJ9ScMISAColWQHk3wY4va/FLPqG7N4L1w3BBtdjIc0A2zRGLNcFDBlxl/CVDHfcqD3CXdLukm/friX6TvnrbTyfAFicYgu0+UtDvfxTL3pRL3u3WTkDvnFK5YXhoazLctNOFrNiiIpCW6dJ7WRYRXuXhz7C0rENHyBtJ0zura1WD5oDbRZ8ON4v1KV4QofWiTFXJpbDgZdEeJJmFmt5HIi+Ny3P5n31WwZpRMHGeHrV23//0CgYEA+2/gYjYWOW3JgMDLX7r8fGPTo1ljkOUHuH98H/a/lE3wnnKKx+2ngRNZX4RfvNG4LLeWTz9plxR2RAqqOTbX8fj/NA/sS4mru9zvzMY1925FcX3WsWKBgKlLryl0vPScq4ejMLSCmypGz4VgLMYZqT4NYIkU2Lo1G1MiDoLy0CcCgYEAwt77exynUhM7AlyjhAA2wSINXLKsdFFF1u976x9kVhOfmbAutfMJPEQWb2WXaOJQMvMpgg2rU5aVsyEcuHsRH/2zatrxrGqLqgxaiqPz4ELINIh1iYK/hdRpr1vATHoebOv1wt8/9qxITNKtQTgQbqYci3KV1lPsOrBAB5S57nsCgYAvw+cagS/jpQmcngOEoh8I+mXgKEET64517DIGWHe4kr3dO+FFbc5eZPCbhqgxVJ3qUM4LK/7BJq/46RXBXLvVSfohR80Z5INtYuFjQ1xJLveeQcuhUxdK+95W3kdBBi8lHtVPkVsmYvekwK+ukcuaLSGZbzE4otcn47kajKHYDQKBgDbQyIbJ+ZsRw8CXVHu2H7DWJlIUBIS3s+CQ/xeVfgDkhjmSIKGX2to0AOeW+S9MseiTE/L8a1wY+MUppE2UeK26DLUbH24zjlPoI7PqCJjl0DFOzVlACSXZKV1lfsNEeriC61/EstZtgezyOkAlSCIH4fGr6tAeTU349Bnt0RtvAoGBAObgxjeH6JGpdLz1BbMj8xUHuYQkbxNeIPhH29CySn0vfhwg9VxAtIoOhvZeCfnsCRTj9OZjepCeUqDiDSoFznglrKhfeKUndHjvg+9kiae92iI6qJudPCHMNwP8wMSphkxUqnXFR3lr9A765GA980818UWZdrhrjLKtIIZdh+X1";
+   $rsa = new Crypt_RSA();
 
-   // $private_key = openssl_pkey_get_private(file_get_contents('./config/private_key.pem'));
+   $private_key_load = file_get_contents('./config/private_key.pem');
 
-    $rsa = new Crypt_RSA();
+   $private_key = trimPrivateKey($private_key_load)[2];
 
-    if($rsa->loadKey($privateKey_pem) != TRUE){
-        echo "Erro loading privateKey";
+    if($rsa->loadKey($private_key) != TRUE){
+        echo "Error loading PrivateKey";
         return;
     };
 
     $rsa->setHash("sha256");
+
     $rsa->setMGFHash("sha256");
 
     // $rsa->signatureMode(Crypt_RSA::$signatureMode);
     $signtureByte = $rsa->sign($data);
 
     return base64_encode($signtureByte);
+}
+
+function trimPrivateKey($stingData){
+ 
+  return explode("-----", (string)$stingData);
+
 }
 
 function createMerchantOrderId() {
